d1dc8d83f27b859b06fa523228d617908c66eadd
bespalov0928 <bespalov0928@mail.ru>
2022-01-09 18:44:20 +0700

    3.4.6.0. Spring test

--- src/test/java/PostControlTest.java
+++ src/test/java/PostControlTest.java
@@ -7,13 +7,18 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.job4j.forum.Main;
+import ru.job4j.forum.service.PostService;
 
 @SpringBootTest(classes = Main.class)
 @AutoConfigureMockMvc
 public class PostControlTest {
+
+    @MockBean
+    private PostService posts;
 
     @Autowired
     MockMvc mockMvc;
@@ -34,6 +39,5 @@ public class PostControlTest {
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(view().name("edit"));
-
     }
 }
